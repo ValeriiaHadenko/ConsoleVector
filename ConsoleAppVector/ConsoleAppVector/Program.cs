0d75fe7c66c122c8116794306895f85596f4873a
ValeriiaHadenko <valeriia.hadenko@nure.ua>
2023-09-28 09:48:42 +0100

    Commit

--- ConsoleAppVector/ConsoleAppVector/Program.cs
+++ ConsoleAppVector/ConsoleAppVector/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Threading;
 using ConsoleAppVector;
 
 Dictionary<string, int> GetResultTask(int vectorCount)
@@ -6,9 +7,9 @@ Dictionary<string, int> GetResultTask(int vectorCount)
     int countEquals = 0;
     int countNotEquals = 0;
 
-    Stopwatch stopwatch = new Stopwatch();
+    Stopwatch stopwatch = new();
 
-    Dictionary<string, int> task = new Dictionary<string, int>();
+    Dictionary<string, int> task = new();
 
     Vector[] vectors1 = Vector.GenerateRandomVectors(vectorCount, min: 0, max: 5);
     Vector[] vectors2 = Vector.GenerateRandomVectors(vectorCount, min: 0, max: 5);
@@ -33,11 +34,67 @@ Dictionary<string, int> GetResultTask(int vectorCount)
     return task;
 }
 
+Dictionary<string, int> MultiThreadedGetResultTask(int vectorCount)
+{
+    int threadCount = Environment.ProcessorCount;
 
-void PrintResultTask(Dictionary<string, int> dict)
+    int countEquals = 0;
+    int countNotEquals = 0;
+
+    Stopwatch stopwatch = new();
+
+    Dictionary<string, int> task = new();
+
+    Vector[] vectors1 = Vector.GenerateRandomVectors(vectorCount, min: 0, max: 5);
+    Vector[] vectors2 = Vector.GenerateRandomVectors(vectorCount, min: 0, max: 5);
+
+
+    object locker = new();
+    stopwatch.Start();
+
+    for (int i = 0; i < threadCount; i++)
+    {
+        Thread myThread = new(() =>
+        {
+            lock (locker)
+            {
+                countEquals = 0;
+                countNotEquals = 0;
+
+                for (int i = 0; i < vectorCount; i++)
+                {
+                    if (Vector.AreEqual(vectors1[i], vectors2[i]))
+                        countEquals++;
+                    else
+                        countNotEquals++;
+
+                }
+            }
+        });
+        myThread.Start();
+    }
+
+
+
+    stopwatch.Stop();
+
+
+
+    task.Add("time", (int)stopwatch.ElapsedMilliseconds);
+    task.Add("vectorCount", vectorCount);
+    task.Add("countEquals", countEquals);
+    task.Add("countNotEquals", countNotEquals);
+
+    return task;
+}
+
+
+
+void PrintResultTask(Dictionary<string, int> dict, string name = "Task result:")
 {
     Console.WriteLine(
-        $"Time: {dict["time"]} ms " +
+        $"{name}" +
+        $"\nTime: {dict["time"]} ms " +
         $"\nNumber of vector elements: {dict["vectorCount"]}" +
         $"\nNumber of equal vectors: {dict["countEquals"]}" +
         $"\nNumber of not equal vectors: {dict["countNotEquals"]}\n"
@@ -47,5 +104,8 @@ void PrintResultTask(Dictionary<string, int> dict)
 
 
 
-PrintResultTask(GetResultTask(600000));
-PrintResultTask(GetResultTask(900000));
+//PrintResultTask(GetResultTask(1000000));
+//PrintResultTask(GetResultTask(2000000));
+
+//PrintResultTask(MultiThreadedGetResultTask(1000000));
+PrintResultTask(MultiThreadedGetResultTask(20000));
